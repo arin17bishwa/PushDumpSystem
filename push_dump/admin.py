6b12c6959a192e762ad3f18b335ca357afafbd76
bishwajit <arin17bishwa@gmail.com>
2025-04-21 17:08:29 +0530

    made model changes and modified admin panel view

--- push_dump/admin.py
+++ push_dump/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
+
 from .models import Push, ResponseStatus
+
 # Register your models here.
 
-admin.site.register(Push)
 admin.site.register(ResponseStatus)
+
+
+@admin.register(Push)
+class PushDumpAdmin(admin.ModelAdmin):
+    list_display = (
+        "content",
+        "response_status_code",
+        "created_at",
+    )  # Columns in list view
+    search_fields = ("content",)  # Searchable fields
+    list_filter = ("created_at",)  # Sidebar filters
+    ordering = ("-created_at",)  # Default order in admin
+    readonly_fields = ("uuid_key", "created_at")  # Prevent edits to these
--- push_dump/models.py
+++ push_dump/models.py
@@ -9,7 +9,16 @@ class Push(models.Model):
     content = models.JSONField()
     headers = models.JSONField()
     query_params = models.JSONField()
+    response_status_code = models.IntegerField()
     uuid_key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    class Meta:
+        verbose_name = "Push"
+        verbose_name_plural = "Pushes"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.id}"
 
 
 class ResponseStatus(models.Model):
@@ -17,7 +26,16 @@ class ResponseStatus(models.Model):
         (status_code, status_code)
         for status_code in sorted((200, 201, 401, 403, 404, 500, 502, 503))
     ]
+
     status_code = models.IntegerField(
         choices=STATUS_OPTIONS,
         default=STATUS_OPTIONS[0][0],
     )
+
+    def __str__(self):
+        return f"{self.status_code}"
+
+    class Meta:
+        verbose_name = "Status code"
+        verbose_name_plural = "Status codes"
+        ordering = ["status_code"]
