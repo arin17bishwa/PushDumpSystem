128f05d74f98c99671fb3ff302dd91295d3daf7e
bishwajit <arin17bishwa@gmail.com>
2025-04-21 17:08:56 +0530

    added a view and endpoint to view, filter, sort pushes

--- push_dump/urls.py
+++ push_dump/urls.py
@@ -1,7 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import store_push
+
+from .views import store_push, json_pushes_view
 
 urlpatterns = [
-    path('push/json/', store_push),
+    path("push/json/", store_push),
+    path("view/", json_pushes_view, name="json_viewer"),
 ]
--- push_dump/views.py
+++ push_dump/views.py
@@ -1,29 +1,51 @@
+import json
+
+from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Push
+from .models import Push, ResponseStatus
 from .utils import get_response_status_code
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def store_push(request):
     try:
         body = request.data
         headers = dict(request.headers)
         query_params = request.query_params.dict()
+        response_status_code = get_response_status_code()
 
         obj = Push.objects.create(
             content=body,
             headers=headers,
-            query_params=query_params
+            query_params=query_params,
+            response_status_code=response_status_code,
         )
 
-        return Response({
-            'message': 'Saved successfully',
-            'id': obj.id,
-            'uuid': obj.uuid_key
-        }, status=get_response_status_code())
+        return Response(
+            {"message": "Saved successfully", "id": obj.id, "uuid": obj.uuid_key},
+            status=response_status_code,
+        )
 
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def json_pushes_view(request):
+
+    objects_data = [
+        {
+            "id": obj.id,
+            "created_at": obj.created_at,
+            "uuid_key": obj.uuid_key,
+            "response_status_code": obj.response_status_code,
+            "content_json": json.dumps(obj.content, indent=2),
+            "headers_json": json.dumps(obj.headers, indent=2),
+            "query_json": json.dumps(obj.query_params, indent=2),
+        }
+        for obj in Push.objects.all()
+    ]
+
+    return render(request, "json_push_viewer_000.html", {"objects": objects_data, "status_codes": [i[0] for i in ResponseStatus.STATUS_OPTIONS]})
